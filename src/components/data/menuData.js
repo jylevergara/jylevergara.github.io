245582dfd488f9e3b1a62630c21b4745e66778e0
Jyle Vergara <jylevergara@gmail.com>
2025-02-18 19:34:16 -0500

    fix responsive design

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -2,7 +2,7 @@ export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"#about", content:"About", icon:"octicon:person-24"},
      {link:"#portfolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     {link:"#cta", content:"Contact", icon:"lucide:mail"}
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
