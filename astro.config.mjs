f73a903538b0c3ec107234a335546325bac20844
Jyle Vergara <jylevergara@gmail.com>
2025-04-09 14:58:03 -0400

    set domain as the value for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jylevergara.github.io/', // Required for sitemap -> Replace with your site's URL
-	output: 'static',
-	integrations: [tailwind(), sitemap()],
-});
+	site: 'https://jylevergara.com',
+  })
