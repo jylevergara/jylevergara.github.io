2de0c10957ba5d5e60e1322e85754a5671383f8c
Jyle Vergara <jylevergara@gmail.com>
2025-04-10 13:31:47 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jylevergara.com',
+	site: 'http://jylevergara.github.io',
 	integrations: [react(), tailwind(), sitemap()],
+	base: 'landing-page-2025'
 });
