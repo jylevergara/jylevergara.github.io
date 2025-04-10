fa58d69dd47787f7617296dfe7fabb7f3719bcb5
Jyle Vergara <jylevergara@gmail.com>
2025-04-10 13:58:54 -0400

    remove static.ym

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
